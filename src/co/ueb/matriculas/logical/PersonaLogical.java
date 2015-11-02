5d28a35b156afddc74b78870c53921c8282a4ee3
Ximena <xgs028@gmail.com>
2015-11-02 13:29:25 -0500

    borré una linea de imprimir

--- src/co/ueb/matriculas/logical/PersonaLogical.java
+++ src/co/ueb/matriculas/logical/PersonaLogical.java
@@ -1,7 +1,5 @@
 package co.ueb.matriculas.logical;
 
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.hibernate.Query;
@@ -15,7 +13,6 @@ public class PersonaLogical {
 	private Session sesion;
 	final Logger log = Logger.getLogger("PersonaLogical");
 
-	@SuppressWarnings("rawtypes")
 	public Persona verificarDatos(Persona persona) {
 		log.info("verificarDatos "+persona);
 		Persona p = null;
@@ -28,39 +25,17 @@ public class PersonaLogical {
 					+ "' and estadoPersona= '1'";
 			log.info(hql);
 			Query query = sesion.createQuery(hql);
-			List resultList = query.list();
-			if (!resultList.isEmpty()) {
-				p = (Persona) resultList.get(0);
-				log.info(p.toString());
+			if (!query.list().isEmpty()) {
+				p = (Persona) query.list().get(0);
+				System.out.println("entr� if");
 			}else{
 				return null;
 			}
+			System.out.println(p.toString());
 			sesion.getTransaction().commit();
 		} catch (Exception e) {
-			sesion.getTransaction().rollback();
 			e.printStackTrace();
-		}
-		return p;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public Persona getPersonaById(BigDecimal id_persona){
-		Persona p = null;
-		try {
-			sesion = HibernateSession.getSf().getCurrentSession();
-			sesion.beginTransaction();
-			String hql = "FROM Persona WHERE id_persona= '" + id_persona
-					+ "' and estadoPersona= '1'";
-			log.info(hql);
-			Query query = sesion.createQuery(hql);
-			p = (Persona) query.uniqueResult();
-			if (p == null) {
-				return null;
-			}
-			sesion.getTransaction().commit();
-		} catch (Exception e) {
 			sesion.getTransaction().rollback();
-			e.printStackTrace();
 		}
 		return p;
 	}
