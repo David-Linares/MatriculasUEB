f777a1c69ddfbda77902feb6b7f19c156c7c7699
David Alexander Linares Villamil <linaresvillamil.davidalexander@gmail.com>
2015-11-02 13:48:33 -0500

    Agregada la función para obtener el promedio

--- src/co/ueb/matriculas/logical/PersonaLogical.java
+++ src/co/ueb/matriculas/logical/PersonaLogical.java
@@ -1,5 +1,6 @@
 package co.ueb.matriculas.logical;
 
+import java.math.BigDecimal;
 import java.util.logging.Logger;
 
 import org.hibernate.Query;
@@ -37,6 +38,28 @@ public class PersonaLogical {
 		} catch (Exception e) {
 			e.printStackTrace();
 			sesion.getTransaction().rollback();
+		}
+		return p;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Persona getPersonaById(BigDecimal id_persona){
+		Persona p = null;
+		try {
+			sesion = HibernateSession.getSf().getCurrentSession();
+			sesion.beginTransaction();
+			String hql = "FROM Persona WHERE id_persona= '" + id_persona
+					+ "' and estadoPersona= '1'";
+			log.info(hql);
+			Query query = sesion.createQuery(hql);
+			p = (Persona) query.uniqueResult();
+			if (p == null) {
+				return null;
+			}
+			sesion.getTransaction().commit();
+		} catch (Exception e) {
+			sesion.getTransaction().rollback();
+			e.printStackTrace();
 		}
 		return p;
 	}
