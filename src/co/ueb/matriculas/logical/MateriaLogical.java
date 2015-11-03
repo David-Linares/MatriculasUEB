c84960d5fdf7bcfb9896904e212896359ab60315
Ximena <xgs028@gmail.com>
2015-11-03 12:47:05 -0500

    nada que funciona

--- src/co/ueb/matriculas/logical/MateriaLogical.java
+++ src/co/ueb/matriculas/logical/MateriaLogical.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import co.ueb.matriculas.model.Carrera;
+import co.ueb.matriculas.model.Facultad;
 import co.ueb.matriculas.model.Materia;
 import co.ueb.matriculas.util.HibernateSession;
 
@@ -41,6 +43,7 @@ public class MateriaLogical {
 
 	@SuppressWarnings("unchecked")
 	public List<Materia> consultarMaterias(){
+	System.out.println("entro a consultar materias - materia logical");
 		List<Materia> materias = new ArrayList<Materia>();
 		String sql = "select m from Materia as m order by m.idMateria";
 		Session session = HibernateSession.getSf().getCurrentSession();
@@ -48,6 +51,8 @@ public class MateriaLogical {
 		Query query = session.createQuery(sql);
 		materias = query.list();
 		session.getTransaction().commit();
+		System.out.println("salio de consultar materias");
+System.out.println(materias);
 		return materias;
 	}
 	
@@ -66,4 +71,22 @@ public class MateriaLogical {
 		}
 	}
 
+	public Materia getMateriaByName(String nombre_materia){
+		Materia m = null;
+		Session session = HibernateSession.getSf().openSession();
+		try {
+			session.beginTransaction();
+			String hql = "FROM Materia WHERE nombre_materia= '" + nombre_materia + "'";
+			Query query = session.createQuery(hql);
+			m = (Materia) query.uniqueResult();
+			if (m == null) {
+				return null;
+			}
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		return m;
+	}
 }
